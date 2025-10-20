009b0307a1d8851af44417ac4d695a28f28cb34f
Kacper0510 <kacper.b.wojciuch@gmail.com>
2025-10-20 12:10:51 +0200

    Update gitignore and Nix shell

--- shell.nix
+++ shell.nix
@@ -10,4 +10,11 @@ pkgs.mkShell {
       ]
     ))
   ];
+  shellHook = ''
+    if [ ! -d ".venv" ]; then
+      python -m venv .venv
+      pip install -r requirements.txt
+    fi
+    source .venv/bin/activate
+  '';
 }
